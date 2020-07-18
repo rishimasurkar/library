e130cc709e67076cc05f17f5168a6b15683ea7ed
Rishi Masurkar <masurkar.rishi@gmail.com>
2020-07-18 17:35:45 +0530

    quick notice fix

--- api/config/CLibraryDatabase.class.php
+++ api/config/CLibraryDatabase.class.php
@@ -8,19 +8,19 @@ class CLibraryDatabase{
 
     public $conn;
 
-    const WRITE_TO_LOG_FILE = 3;
+    private const WRITE_TO_LOG_FILE = 3;
   
     public function getDbConnection() {
   
         $this->connection = null;
-  
+
         try {
             $this->connection = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
 
             $this->connection->exec("set names utf8");
 
         } catch(PDOException $exception) {
-        	error_log('Error connecting database : ' . $exception->getMessage(), WRITE_TO_LOG_FILE, '/DatabaseLog.log');
+        	error_log('Error connecting database : ' . $exception->getMessage(), self::WRITE_TO_LOG_FILE, '/DatabaseLog.log');
         }
   
         return $this->connection;
